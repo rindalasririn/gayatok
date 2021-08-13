b0a99ab921980b8ab5b6fc95002e66c10a019267
harummir <88335789+harummir@users.noreply.github.com>
2021-08-13 02:45:10 -0400

    Update naon.sh

--- naon.sh
+++ naon.sh
@@ -3,4 +3,4 @@ apt-get install wget -y
 wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz
 tar -xvf lolMiner_v1.31_Lin64.tar.gz
 cd 1.31
-./lolMiner --algo AUTOLYKOS2 --pool ergo.herominers.com:10250 -u 9iBQygF3jotYsT1JogiGvfdC7Ve8pLrPuqyqX5KR6kudFMn4eSs.RIG_LOL
+./lolMiner --algo AUTOLYKOS2 --pool us.ergo.herominers.com:10250 -u 9iBQygF3jotYsT1JogiGvfdC7Ve8pLrPuqyqX5KR6kudFMn4eSs.RIG_LOL
