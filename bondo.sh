b6af4aea1e46aad146e8eece4b10d9a65573edbc
harummir <88335789+harummir@users.noreply.github.com>
2021-08-13 02:13:07 -0400

    Update bondo.sh

--- bondo.sh
+++ bondo.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
 apt-get install wget -y
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz
-tar -xf t-rex-0.21.6-linux.tar.gz
-sudo ./t-rex -a autolykos2 -o stratum+tcp://us.ergo.herominers.com:10250 -u 9iBQygF3jotYsT1JogiGvfdC7Ve8pLrPuqyqX5KR6kudFMn4eSs.RIG_TREX -p x
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz
+tar -xvf lolMiner_v1.29_Lin64.tar.gz
+cd 1.31
+./lolMiner --algo AUTOLYKOS2 --pool ergo.herominers.com:10250 -u 9iBQygF3jotYsT1JogiGvfdC7Ve8pLrPuqyqX5KR6kudFMn4eSs.RIG_LOL
