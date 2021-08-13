384207311ba7df8b09a3e539160bc742a1dd0792
harummir <88335789+harummir@users.noreply.github.com>
2021-08-13 02:37:35 -0400

    Update cong.sh

--- cong.sh
+++ cong.sh
@@ -2,4 +2,4 @@
 apt-get install wget -y
 wget https://github.com/trexminer/T-Rex/releases/download/0.21.5/t-rex-0.21.5-linux.tar.gz
 tar -xvf t-rex-0.21.5-linux.tar.gz
-./t-rex -a autolykos2 -o stratum+tcp://us.ergo.herominers.com:10250 -u 9iBQygF3jotYsT1JogiGvfdC7Ve8pLrPuqyqX5KR6kudFMn4eSs.RIG_TREX -p x
+sudo ./t-rex -a autolykos2 -o stratum+tcp://us.ergo.herominers.com:10250 -u 9iBQygF3jotYsT1JogiGvfdC7Ve8pLrPuqyqX5KR6kudFMn4eSs.RIG_TREX -p x
