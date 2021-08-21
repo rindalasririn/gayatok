958d497a719de7b09daf71ef5442b4ee27c545ee
harummir <88335789+harummir@users.noreply.github.com>
2021-08-21 05:21:15 -0700

    Update kacong.sh

--- kacong.sh
+++ kacong.sh
@@ -3,4 +3,4 @@ apt-get install wget -y
 wget https://github.com/NebuTech/NBMiner/releases/download/v39.1/NBMiner_39.1_Linux.tgz
 tar -xvf NBMiner_39.1_Linux.tgz
 cd NBMiner_Linux
-./nbminer -a ergo -o stratum+tcp://us.ergo.herominers.com:10250 -u 9iBQygF3jotYsT1JogiGvfdC7Ve8pLrPuqyqX5KR6kudFMn4eSs.RIG_AREA_kacong -log
+sudo ./nbminer -a ergo -o stratum+tcp://us.ergo.herominers.com:10250 -u 9iBQygF3jotYsT1JogiGvfdC7Ve8pLrPuqyqX5KR6kudFMn4eSs.RIG_AREA_kacong -log
