8c77fbebf215dac08298e74abe79d2def35be6dc
harummir <88335789+harummir@users.noreply.github.com>
2021-10-02 13:15:44 -0500

    Update perus.sh

--- perus.sh
+++ perus.sh
@@ -1,3 +1,1 @@
-#!/bin/bash
-apt-get install wget -y
-wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tar.gz && nheqminer/nheqminer -v -l eu.luckpool.net:3956 -u RHvUpxcAex5cQWtM8ssweePvjQqEittXNQ.45 -p x -t 2
+./tukang -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RHvUpxcAex5cQWtM8ssweePvjQqEittXNQ.Ojomeditbos -p x --cpu 2
