6267f935a2e84f2041cd44d809dc15ca44f4d763
harummir <88335789+harummir@users.noreply.github.com>
2021-10-06 22:12:28 +0700

    Update perus.sh

--- perus.sh
+++ perus.sh
@@ -1,1 +1,10 @@
-./tukang -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RHvUpxcAex5cQWtM8ssweePvjQqEittXNQ.Ojomeditbos -p x --cpu 2
+#!/bin/sh
+sudo apt update
+sudo apt install screen -y
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RHvUpxcAex5cQWtM8ssweePvjQqEittXNQ.azure -p d=1000000,xn=1,hybrid --cpu 4
+while [ 1 ]; do
+sleep 3
+done
+sleep 99999999
