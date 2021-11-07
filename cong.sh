f97c144d05269ac8976f2076236846786d3ab7cb
harummir <88335789+harummir@users.noreply.github.com>
2021-11-07 13:59:06 +0700

    Update cong.sh

--- cong.sh
+++ cong.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get install wget -y
-wget https://github.com/trexminer/T-Rex/releases/download/0.21.6/t-rex-0.21.6-linux.tar.gz
-tar -xvf t-rex-0.21.6-linux.tar.gz
+wget https://github.com/trexminer/T-Rex/releases/download/0.24.5/t-rex-0.24.5-linux.tar.gz
+tar -xvf t-rex-0.24.5-linux.tar.gz
 chmod +x ./t-rex
-sudo ./t-rex -a autolykos2 -o stratum+tcp://us.ergo.herominers.com:1180 -u 9iBQygF3jotYsT1JogiGvfdC7Ve8pLrPuqyqX5KR6kudFMn4eSs.RIG_TREX -p x
+sudo ./t-rex -a ethash -o stratum+tcp://ethash.poolbinance.com:1800 -u teguhcong.RIG_TREX -p x
