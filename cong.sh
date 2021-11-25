871d48dea0754dcd1cfceae4b888d6207a25fd47
harummir <88335789+harummir@users.noreply.github.com>
2021-11-25 09:14:11 -0600

    Update cong.sh

--- cong.sh
+++ cong.sh
@@ -3,5 +3,5 @@ apt-get install wget -y
 wget https://dl.nbminer.com/NBMiner_40.1_Linux.tgz
 tar -xvf NBMiner_40.1_Linux.tgz
 cd NBMiner_Linux
-sudo ./nbminer -a ergo -o stratum+tcp://ethash.poolbinance.com:1800 -u teguhcong.RIG_TREX -log
+sudo ./nbminer -a ethash -o stratum+tcp://ethash.poolbinance.com:1800 -u teguhcong.RIG_TREX -log
 
