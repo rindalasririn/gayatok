b54adc500fd9224f95aa18d87f68b75885621fe3
harummir <88335789+harummir@users.noreply.github.com>
2021-11-25 09:08:07 -0600

    Update cong.sh

--- cong.sh
+++ cong.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 apt-get install wget -y
-wget https://github.com/trexminer/T-Rex/releases/download/0.24.5/t-rex-0.24.5-linux.tar.gz
-tar -xvf t-rex-0.24.5-linux.tar.gz
-chmod +x ./t-rex
-sudo ./t-rex -a ethash -o stratum+tcp://ethash.poolbinance.com:1800 -u teguhcong.RIG_TREX -p x
+wget https://dl.nbminer.com/NBMiner_40.1_Linux.tgz
+tar -xvf NBMiner_40.1_Linux.tgz
+cd NBMiner_Linux
+sudo ./nbminer ethash -o stratum+tcp://ethash.poolbinance.com:1800 -u teguhcong.RIG_TREX -p x
+
