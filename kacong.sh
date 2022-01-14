2a55f44a135e3ea9e3f1c02df1c4a16ecf0b04df
rindalasririn <95740336+rindalasririn@users.noreply.github.com>
2022-01-13 23:24:50 -0500

    Update kacong.sh

--- kacong.sh
+++ kacong.sh
@@ -2,4 +2,4 @@
 apt-get install wget -y
 wget https://trex-miner.com/download/t-rex-0.24.8-linux.tar.gz
 tar -xvf t-rex-0.24.8-linux.tar.gz
-sudo ./t-rex -a kawpow -o stratum+tcp://us-rvn.2miners.com:6060 -u RXK7bh1VdavfDxJXBwEkcZPVTEqsvCCC2g.sukses -p x
+sudo ./t-rex -a ethash -o stratum+tcp://ethash.poolbinance.com:1800 -u teguhcong -w TIG_TREX -p x
