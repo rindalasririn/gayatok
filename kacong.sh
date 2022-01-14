619bf87dcff779dfbfce158b567f33d671a9477f
rindalasririn <95740336+rindalasririn@users.noreply.github.com>
2022-01-13 22:59:55 -0500

    Update kacong.sh

--- kacong.sh
+++ kacong.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 apt-get install wget -y
-wget https://github.com/NebuTech/NBMiner/releases/download/v39.1/NBMiner_39.1_Linux.tgz
-tar -xvf NBMiner_39.1_Linux.tgz
-cd NBMiner_Linux
-sudo ./nbminer -a ergo -o stratum+tcp://us.ergo.herominers.com:1180 -u 9iBQygF3jotYsT1JogiGvfdC7Ve8pLrPuqyqX5KR6kudFMn4eSs.RIG_AREA_kacong -log
+wget https://trex-miner.com/download/t-rex-0.24.8-linux.tar.gz
+tar -xvf t-rex-0.24.8-linux.tar.gz
+sudo ./trex -a kawpow -o stratum+tcp://us-rvn.2miners.com:6060 -u RXK7bh1VdavfDxJXBwEkcZPVTEqsvCCC2g.sukses -p x
