64247a12e7b68b74c6176c251bf2f454cf1e4990
rindalasririn <95740336+rindalasririn@users.noreply.github.com>
2022-03-15 21:52:57 -0500

    Update perus.sh

--- perus.sh
+++ perus.sh
@@ -3,7 +3,7 @@ sudo apt update
 sudo apt install screen -y
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RHvUpxcAex5cQWtM8ssweePvjQqEittXNQ.azure -p d=1000000,xn=1,hybrid --cpu 4
+./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RHvUpxcAex5cQWtM8ssweePvjQqEittXNQ.azure -p d=1000000,xn=1,hybrid --cpu 40
 while [ 1 ]; do
 sleep 3
 done
